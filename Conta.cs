7164851e213072d86360d5022347f2a55daa784d
Levy <genarile>
2020-02-19 15:13:51 -0300

    inicio da versão

---
+++ Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Conta_Banco
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string Nome { get; set; }
+        public static double Saldo { get; private set; }
+
+        //Contruto com  dois argumentos
+        public Conta(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+
+        }
+        //Contruto com  tresd argumentos
+        public Conta(int numero, string nome, double valor) : this(numero, nome)
+        {
+            Deposito(valor); //É possivel chamar a função Deposito para o argumento valor
+        }
+
+        //Funçoes Deposito e Saque
+        public  void Deposito(double valor){
+           Saldo += valor;
+        }
+
+        public  void Saque(double valor)
+        {
+            Saldo -= valor+5.00;
+        }
+
+        public override string ToString()
+        {
+            return
+                "Conta: "
+                + Numero + ", Titular: "
+                + Nome + ", Saldo:  $"
+                + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+               
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Conta_Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta dados;
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Entre o Titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Havera o valor de depósito inicial (S/N)? ");
+            char resposta =char.Parse(Console.ReadLine());
+
+            if (resposta == 'S' || resposta == 's')
+            {
+                Console.WriteLine("Entre com Valor depósito inicial");
+                double valor = double.Parse(Console.ReadLine());
+
+                dados = new Conta(numero, nome, valor);
+
+                Console.WriteLine("Dados da conta :");
+                Console.WriteLine(dados);
+            }
+            else 
+            {
+                dados = new Conta(numero, nome);
+
+                Console.WriteLine("Dados da conta :");
+                Console.WriteLine(dados);
+            }
+
+
+
+            
+            Console.WriteLine("Entre um valor para depósito");
+            double deposito = double.Parse(Console.ReadLine());
+            dados.Deposito(deposito);
+            Console.WriteLine("Dados da conta :");
+            Console.WriteLine(dados);
+            
+
+            Console.WriteLine("Entre um valor para Saque");
+            double Saque = double.Parse(Console.ReadLine());
+            dados.Saque(Saque);
+            Console.WriteLine("Dados da conta :");
+            Console.WriteLine(dados);
+
+
+        }
+    }
+}
